4e1c8c24b755871db1330f0e33b8f4c702eb1dc1
fogelt <fogeltt@gmail.com>
2026-01-26 18:41:39 +0100

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
-import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', //change to '/' on paid domain
+  base: '/BankWebUI', //change to '/' on paid domain
   plugins: [
     react(),
-
-    Sitemap({
-      hostname: 'https://myeliefoto.se', // Replace with your actual domain
-      dynamicRoutes: [
-        '/',
-        '/weddings',
-        '/portraits',
-        '/articles',
-        '/about',
-      ],
-      exclude: ['/admin', '/sign-in', '/sign-up']
-    }),
   ],
 
   css: {
